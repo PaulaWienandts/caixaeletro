8a2c191d7772957054f8df618ccedf7477556123
Paula Wienandts <paulawienandts@gmail.com>
2016-06-23 21:23:38 -0300

    segundo envio(código correto)

--- caixa/src/caixa/caixaeletronico.java
+++ caixa/src/caixa/caixaeletronico.java
@@ -8,27 +8,26 @@ int n50=0;
 int n20=0;
 int n10=0;
 int n2=0;
-
-
-double saca= 784 ;{
- if (saca>100) 
+int saca= 784 ;
+temp=saca;
+ if (saca>100) {
 	 n100 = (int) (saca / 100);
 	temp=(int) (saca%100);
 }
-	 if (saca>50){
-		n50 =(int) (saca/50);
+	 if (temp>50){
+		n50 =(int) (temp/50);
 	 temp=temp%50;
 	 }
-	 if (saca>20){
-		n20=(int) (saca-20);
+	 if (temp>20){
+		n20=(int) (temp/20);
 	temp=temp%20;
 	 }
-	 if (saca>10){
-		n10=(int) (saca-10);
+	 if (temp>10){
+		n10=(int) (temp/10);
 		temp=temp%10;
 	 }
-	 if (saca>2){
-			n2=(int) (saca-2);
+	 if (temp>2){
+			n2=(int) (temp/2);
 		temp=temp%2;
 		
 	 }
